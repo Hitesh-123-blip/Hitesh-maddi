720b3b0f156b53e2f7955f738d7b8f11f7014d22
Hitesh-123-blip <61444723+Hitesh-123-blip@users.noreply.github.com>
2020-02-24 20:37:09 -0800

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,7 @@
+public Class AddTwoNumbers{
+  public static void main(String[]args){
+   int num1=5,num2=15,sum;
+    sum=num1+num2;
+     System.out.Println("sum of these numbres:"+sum);
+     }
+  }
