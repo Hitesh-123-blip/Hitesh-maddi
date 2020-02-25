08dd77c2f037f2941e80e681007681053f2f4ba7
Hitesh-123-blip <61444723+Hitesh-123-blip@users.noreply.github.com>
2020-02-24 20:38:32 -0800

    Delete AddTwoNumbers.java

--- AddTwoNumbers.java
+++
@@ -1,7 +0,0 @@
-public Class AddTwoNumbers{
-  public static void main(String[]args){
-   int num1=5,num2=15,sum;
-    sum=num1+num2;
-     System.out.Println("sum of these numbres:"+sum);
-     }
-  }
